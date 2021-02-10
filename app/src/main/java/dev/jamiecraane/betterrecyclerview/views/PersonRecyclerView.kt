d866629061c8b8cd19e0c1cda5c22cb3406bc3a5
jamiecraane <jcraane@gmail.com>
2021-02-10 23:26:58 +0100

    Remove unused method

--- app/src/main/java/dev/jamiecraane/betterrecyclerview/views/PersonRecyclerView.kt
+++ app/src/main/java/dev/jamiecraane/betterrecyclerview/views/PersonRecyclerView.kt
@@ -7,12 +7,6 @@ import dev.jamiecraane.betterrecyclerview.BetterRecyclerView
 import nl.capaxambi.wordtranslator.androidapp.components.DragAndDropConfig
 
 class PersonRecyclerView(context: Context, attributeSet: AttributeSet? = null) : BetterRecyclerView<PersonModel>(context, attributeSet) {
-    var persons: List<PersonModel> = emptyList()
-        set(value) {
-            field = value
-            setItems(persons) { PersonView(context) }
-        }
-
     init {
         dragAndDropConfig = DragAndDropConfig(
             dragUsingDragHandle = true,
